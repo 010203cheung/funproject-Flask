132a8a4860f5afcbdf2c0eab9c54b674c4e9858d
010203cheung <010203cheung@gmail.com>
2026-04-26 13:01:52 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+# app.py - flask app
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
